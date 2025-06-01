cafafffcfcf329c5aa64e3c32f7d69e3efc28868
GannaKov <zlatta2000@gmail.com>
2025-06-01 21:36:22 +0200

    added delete account and logout

--- app/instructor/profile.tsx
+++ app/instructor/profile.tsx
@@ -8,11 +8,37 @@ import InputField from "@/components/forms/InputFieldProfile";
 import { useAuthStore } from "@/store/authStore";
 
 const Profile = () => {
+  const instructorProfile = useAuthStore((state) => state.profile);
+  const signOut = useAuthStore((state) => state.signOut);
+
+  const deleteAccount = useAuthStore((state) => state.deleteAccount);
+
+  const handleLogout = async () => {
+    try {
+      signOut();
+      //change it to do nice
+      alert("You have been logged out successfully.");
+      router.push("/");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+  const handleDeleteAccount = async () => {
+    try {
+      deleteAccount();
+      //change it to do nice
+      alert("You have been deleted successfully.");
+      router.push("/");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+
   const handleSubmit = () => {};
   return (
     <View className="flex-1 bg-black container py-8">
       <Pressable
-        onPress={handleSubmit}
+        onPress={handleLogout}
         className="bg-green w-full py-3 mt-16 mb-8 rounded-xl"
       >
         <Text className="text-center text-black text-lg font-semibold font-manrope">
@@ -20,7 +46,7 @@ const Profile = () => {
         </Text>
       </Pressable>
       <Pressable
-        onPress={handleSubmit}
+        onPress={handleDeleteAccount}
         className="bg-warning w-full py-3 mt-16 mb-8 rounded-xl"
       >
         <Text className="text-center text-black text-lg font-semibold font-manrope">
