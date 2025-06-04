7781cd7e9ea291027ab9cdc8005d012f8d6a526b
GannaKov <zlatta2000@gmail.com>
2025-06-04 12:16:41 +0200

    save

--- app/instructor/test.tsx
+++ app/instructor/test.tsx
@@ -22,20 +22,27 @@ const Test = () => {
   return (
     <SafeAreaView className="flex-1  container">
       <View className="py-8 ">
-        {" "}
         <Text className="text-white text-lg font-bold">
           Список Вільних Інструкторів
         </Text>
-        <FlatList
-          data={instructors}
-          renderItem={({ item }) => (
-            <View className="p-4 border-b border-gray-200">
-              <Text className="text-lg text-white font-bold">{item.name}</Text>
-              <Text className="text-sm text-white">{item.phone}</Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.uidInstructor}
-        />
+        {instructors.length > 0 ? (
+          <FlatList
+            data={instructors}
+            renderItem={({ item }) => (
+              <View className="p-4 border-b border-gray-200">
+                <Text className="text-lg text-white font-bold">
+                  {item.name}
+                </Text>
+                <Text className="text-sm text-white">{item.phone}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item.uidInstructor}
+          />
+        ) : (
+          <Text className="text-sm text-white">
+            Нажаль зараз немає вільних інструкторів
+          </Text>
+        )}
       </View>
     </SafeAreaView>
   );
